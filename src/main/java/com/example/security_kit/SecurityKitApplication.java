c8259929d8a1e786909f9fb8f1dfa75636db8641
brenoloa <contatobrenoloa@gmail.com>
2026-03-29 12:36:17 -0300

    initial commit.

---
+++ src/main/java/com/example/security_kit/SecurityKitApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example")
+public class SecurityKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityKitApplication.class, args);
+	}
+
+}
